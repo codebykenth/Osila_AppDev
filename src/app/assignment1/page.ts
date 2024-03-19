8b05c55cbf6f5bc2da24e71f4aa19073761c6b9a
kenth <kenth@LAPTOP-832N4QBD>
2024-03-20 01:23:28 +0800

    Activity 1 Commit

---
+++ src/app/assignment1/page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment1',
+  templateUrl: 'page.html',
+  styleUrls: ['page.scss'],
+})
+export class CustomPage {
+  groupName = 'Xenolith';
+  constructor() {}
+}
